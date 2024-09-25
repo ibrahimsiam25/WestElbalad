f86eeb7f5f80c4172ad625358239cd84fbad470f
itiswd <itiswdev97@gmail.com>
2024-09-25 18:05:11 +0300

    Add Recycle Sign Image and Update App Assets
    - Added a new recycle sign image (`recycle-sign.png`) to the assets directory.
    - Updated `app_assets.dart` to include the new recycle sign image and a plus icon.
    - Refactored `get_user.dart` to improve import order and readability.
    - Modified `custom_home_app_bar.dart` to wrap the app bar with `SafeArea` and update the subtitle text.
    - Updated `home_view_body.dart` to include new sections for displaying new and used phones, and added a custom title widget.
    - Improved the layout and alignment of phone listings in `home_view_body.dart`.

--- lib/features/home/presentation/views/widgets/custom_home_app_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_home_app_bar.dart
@@ -1,31 +1,42 @@
-import 'package:svg_flutter/svg.dart';
 import 'package:flutter/material.dart';
-import '../../../../../core/utils/app_styles.dart';
-import '../../../../../core/functions/get_user.dart';
-import '../../../../../core/constants/app_colors.dart';
 import 'package:west_elbalad/core/constants/app_assets.dart';
 
-
+import '../../../../../core/constants/app_colors.dart';
+import '../../../../../core/utils/app_styles.dart';
 
 class CustomHomeAppBar extends StatelessWidget {
   const CustomHomeAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      trailing: Container(
+    return SafeArea(
+      child: ListTile(
+        trailing: Container(
           width: 50,
           height: 50,
           padding: const EdgeInsets.all(12),
           decoration: const ShapeDecoration(
-              shape: OvalBorder(), color: AppColors.white),
-          child:
-             Icon(Icons.shopping_cart, color: AppColors.black, size: 20)),
-      leading: Image.asset(AppAssets.profile),
-      title: Text("صباح الخير !..",
-          style:AppStyles.semiBold16.copyWith(color: AppColors.grey)),
-      subtitle: Text(getUser().name,
-          style: AppStyles.semiBold16.copyWith(color: AppColors.black)),
+            shape: CircleBorder(),
+            color: AppColors.white,
+          ),
+          child: Icon(
+            Icons.shopping_cart,
+            color: AppColors.black,
+            size: 20,
+          ),
+        ),
+        leading: Image.asset(AppAssets.profile),
+        title: Text(
+          "صباح الخير !..",
+          style: AppStyles.semiBold16.copyWith(color: AppColors.grey),
+        ),
+        subtitle: Text(
+          "ابراهيم ثروت",
+          style: AppStyles.semiBold16.copyWith(color: AppColors.black),
+        ),
+        //   subtitle: Text(getUser().name,
+        //       style: AppStyles.semiBold16.copyWith(color: AppColors.black)),
+      ),
     );
   }
 }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -3,12 +3,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:west_elbalad/core/constants/app_assets.dart';
 import 'package:west_elbalad/core/constants/app_consts.dart';
+import 'package:west_elbalad/core/utils/app_styles.dart';
+import 'package:west_elbalad/core/widgets/custom_title.dart';
 import 'package:west_elbalad/features/home/domian/entites/phone_entites.dart';
-import 'package:west_elbalad/features/home/presentation/views/widgets/filter_list.dart';
-import 'package:west_elbalad/features/home/presentation/views/widgets/home_appbar.dart';
-import 'package:west_elbalad/features/home/presentation/views/widgets/selected_phones.dart';
-import 'package:west_elbalad/features/home/presentation/views/widgets/custom_home_app_bar.dart';
 import 'package:west_elbalad/features/home/presentation/manager/phones_filter/filter_cubit.dart';
+import 'package:west_elbalad/features/home/presentation/views/widgets/custom_home_app_bar.dart';
+import 'package:west_elbalad/features/home/presentation/views/widgets/filter_list.dart';
+import 'package:west_elbalad/features/home/presentation/views/widgets/selected_phones.dart';
 
 class HomeViewBody extends StatelessWidget {
   final List<PhoneEntites> phones;
@@ -23,105 +24,202 @@ class HomeViewBody extends StatelessWidget {
       create: (context) => FilterListCubit(),
       child: CustomScrollView(
         slivers: [
+          SliverToBoxAdapter(child: CustomHomeAppBar()),
           SliverToBoxAdapter(
-            child: CustomHomeAppBar()
-          ),
-    SliverToBoxAdapter(
-      child: SizedBox(
-        height: MediaQuery.of(context).size.height * 0.27,
-        child: SingleChildScrollView(
-          padding: EdgeInsets.symmetric(horizontal:kHorizontalPadding, vertical: 16.0),
-          scrollDirection: Axis.horizontal,
-          child: Row(
-            children: [  
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.3,
-                child: AspectRatio(
-                  aspectRatio: 1321 / 736,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8.0),
-                    child: Image.asset(AppAssets.offer1)),
-                ),
-              ),
-              SizedBox(width: 8), // Add spacing between images
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.3,
-                child: AspectRatio(
-                  aspectRatio: 1321 / 736,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8.0),
-                    child: Image.asset(AppAssets.offer2)),
-                ),
-              ),
-           
-            ],
-          ),
-        ),
-      ),
-    ),
-           SliverToBoxAdapter(
-             child: Column(
-                       children: [
-              //AppBar
-          
-
-              BlocBuilder<FilterListCubit, int>(
-                builder: (context, state) {
-                  final orderedPhones = [
-                    'all',
-                    'samsung',
-                    'oppo',
-                    'realme',
-                    'mi',
-                    'nokia',
-                    ...phones.map((phone) => phone.type).toList()
-                  ].toSet().toList();
-                  final selectedPhones = phones
-                      .where(
-                        (phone) => phone.type == orderedPhones[state],
-                      )
-                      .map((phone) => SelectedPhones(phones: phone))
-                      .toList();
-                  return Column(
-                    children: [
-                      //Filters
-                      Filters(
-                        phones: phones,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.27,
+              child: SingleChildScrollView(
+                padding: EdgeInsets.symmetric(
+                    horizontal: kHorizontalPadding, vertical: 16.0),
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.3,
+                      child: AspectRatio(
+                        aspectRatio: 1321 / 736,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8.0),
+                            child: Image.asset(AppAssets.offer1)),
                       ),
-                      SizedBox(height: 16.0.h),
-                      //Selected phones
-                      Wrap(
-                        spacing: 16.0.w,
-                        children: orderedPhones[state] == 'all'
-                            ? [
-                                ...phones.map(
-                                    (phone) => SelectedPhones(phones: phone)),
-                              ]
-                            : [
-                                ...selectedPhones,
-                                //For alighnment and refresh
-                                if (selectedPhones.length == 1)
-                                  Container(
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: kHorizontalPadding),
-                                    child: SizedBox(
-                                      width: 128.0.w,
-                                      height: 300.0.w,
-                                    ),
-                                  ),
-                              ],
-                      )
-                    ],
-                  );
-                },
+                    ),
+                    SizedBox(width: 8),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.3,
+                      child: AspectRatio(
+                        aspectRatio: 1321 / 736,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(8.0),
+                            child: Image.asset(AppAssets.offer2)),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-                       ],
-                     ),
-           ),
-        ]
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: BlocBuilder<FilterListCubit, int>(
+              builder: (context, state) {
+                final orderedPhones = [
+                  'all',
+                  'samsung',
+                  'oppo',
+                  'realme',
+                  'mi',
+                  'nokia',
+                  ...phones.map((phone) => phone.type).toList()
+                ].toSet().toList();
+                final selectedPhones = phones
+                    .where(
+                      (phone) =>
+                          phone.type == orderedPhones[state] &&
+                          (phone.status == 'new' || phone.status == 'جديد'),
+                    )
+                    .map((phone) => SelectedPhones(phones: phone))
+                    .toList();
+                final usedPhones = phones
+                    .where(
+                      (phone) =>
+                          phone.type == orderedPhones[state] &&
+                          (phone.status == 'used' || phone.status == 'مستعمل'),
+                    )
+                    .map((phone) => SelectedPhones(phones: phone))
+                    .toList();
+                return Column(
+                  children: [
+                    //New phones
+                    Column(
+                      children: [
+                        //Filters
+                        Filters(
+                          phones: phones,
+                        ),
+                        SizedBox(height: 16.0.h),
+                        CustomTitle(title: 'الأجهزة الجديدة'),
+                        SizedBox(height: 12.0.h),
+                        //Selected phones
+                        Wrap(
+                          spacing: 16.0.w,
+                          children: orderedPhones[state] == 'all'
+                              ? [
+                                  ...phones
+                                      .where(
+                                        (phone) => (phone.status == 'جديد' ||
+                                            phone.status == 'new'),
+                                      )
+                                      .map((phone) =>
+                                          SelectedPhones(phones: phone)),
+                                  //For alighnment and refresh
+                                  if (selectedPhones.length == 1)
+                                    Container(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: kHorizontalPadding),
+                                      child: SizedBox(
+                                        width: 128.0.w,
+                                      ),
+                                    ),
+                                ]
+                              : [
+                                  ...selectedPhones,
+                                  //For alighnment and refresh
+                                  if (selectedPhones.length == 1)
+                                    Container(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: kHorizontalPadding),
+                                      child: SizedBox(
+                                        width: 128.0.w,
+                                      ),
+                                    ),
+
+                                  //Empty
+                                  if (selectedPhones.isEmpty)
+                                    Container(
+                                      padding: EdgeInsets.only(
+                                          bottom: kHorizontalPadding),
+                                      child: SizedBox(
+                                        width: 200.0.w,
+                                        height: 128.0.h,
+                                        child: Center(
+                                          child: Text(
+                                            'لا توجد اجهزة جديدة\nمن هذا النوع.',
+                                            textAlign: TextAlign.center,
+                                            style: AppStyles.semiBold16,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 16.0.h),
+                    //Used phones
+                    Column(
+                      children: [
+                        CustomTitle(title: 'الأجهزة المستعملة'),
+                        SizedBox(height: 12.0.h),
+                        //Selected phones
+                        Wrap(
+                          spacing: 16.0.w,
+                          children: orderedPhones[state] == 'all'
+                              ? [
+                                  ...phones
+                                      .where(
+                                        (phone) => (phone.status == 'مستعمل' ||
+                                            phone.status == 'used'),
+                                      )
+                                      .map((phone) =>
+                                          SelectedPhones(phones: phone)),
+                                  //For alighnment and refresh
+                                  if (usedPhones.length == 1)
+                                    Container(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: kHorizontalPadding),
+                                      child: SizedBox(
+                                        width: 128.0.w,
+                                      ),
+                                    ),
+                                ]
+                              : [
+                                  ...usedPhones,
+                                  //For alighnment and refresh
+                                  if (usedPhones.length == 1)
+                                    Container(
+                                      padding: EdgeInsets.symmetric(
+                                          horizontal: kHorizontalPadding),
+                                      child: SizedBox(
+                                        width: 128.0.w,
+                                      ),
+                                    ),
+                                  //Empty
+                                  if (usedPhones.isEmpty)
+                                    Container(
+                                      padding: EdgeInsets.only(
+                                          bottom: kHorizontalPadding),
+                                      child: SizedBox(
+                                        width: 200.0.w,
+                                        height: 128.0.h,
+                                        child: Center(
+                                          child: Text(
+                                            'لا توجد اجهزة مستعملة\nمن هذا النوع.',
+                                            textAlign: TextAlign.center,
+                                            style: AppStyles.semiBold16,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                ],
+                        ),
+                      ],
+                    )
+                  ],
+                );
+              },
+            ),
+          )
+        ],
       ),
     );
   }
 }
-
-
