809a3ea12e7edbebbc06f287c09c952430518b84
itiswd <itiswdev97@gmail.com>
2024-09-26 15:05:38 +0300

    Profile UI Done.

--- lib/features/profile/presentation/views/profile_view.dart
+++ lib/features/profile/presentation/views/profile_view.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../../../core/service/get_it_service.dart';
-import '../../../admin/presentation/manager/image_picker/image_picker_cubit.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:west_elbalad/core/widgets/custom_app_bar.dart';
 import 'package:west_elbalad/features/profile/presentation/manager/cubit/profile_cubit.dart';
 import 'package:west_elbalad/features/profile/presentation/views/widgets/profile_view_body_bloc_consumer.dart';
+
+import '../../../../core/service/get_it_service.dart';
+import '../../../admin/presentation/manager/image_picker/image_picker_cubit.dart';
 
 class ProfileView extends StatelessWidget {
   const ProfileView({super.key});
@@ -20,6 +23,15 @@ class ProfileView extends StatelessWidget {
         ),
       ],
       child: Scaffold(
+        extendBodyBehindAppBar: true,
+        extendBody: true,
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(80.0.h),
+          child: CustomAppBar(
+            title: "الملف الشخصي",
+            backButton: false,
+          ),
+        ),
         body: const ProfileViewBodyBlocConsumer(),
       ),
     );
