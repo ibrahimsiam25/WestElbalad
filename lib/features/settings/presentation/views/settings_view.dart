3ccea2e5f1fe2e45c77526585bf60505bde9c0e8
itiswd <itiswdev97@gmail.com>
2024-09-26 15:19:28 +0300

    Settings UI Done.

--- lib/features/settings/presentation/views/settings_view.dart
+++ lib/features/settings/presentation/views/settings_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:west_elbalad/core/widgets/custom_app_bar.dart';
 import 'package:west_elbalad/features/settings/presentation/views/widgets/settings_view_body.dart';
 
 class SettingsView extends StatelessWidget {
@@ -6,7 +8,14 @@ class SettingsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(80.0.h),
+        child: CustomAppBar(
+          title: "الاعدادات",
+          backButton: false,
+        ),
+      ),
       body: SettingsViewBody(),
     );
   }
