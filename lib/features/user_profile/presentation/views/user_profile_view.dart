1d6f3637b21fdd3f787ed8125ffd0fb31b021946
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2024-11-27 23:01:54 +0200

    Add loading indicator and success/failure messages to UserProfileView
    Enhanced the UserProfileView by adding a loading indicator using ModalProgressHUD and handling success and failure states of the UploadUserImageCubit to display appropriate messages.

--- lib/features/user_profile/presentation/views/user_profile_view.dart
+++ lib/features/user_profile/presentation/views/user_profile_view.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:west_elbalad/features/user_profile/presentation/manager/upload_user_image/upload_user_image_cubit.dart';
 import 'package:west_elbalad/features/user_profile/presentation/views/widgets/user_profile_view_body.dart';
 
+import '../../../../core/functions/build_message_bar.dart';
 import '../../../../core/service/get_it_service.dart';
 import '../../../admin/presentation/manager/image_picker/image_picker_cubit.dart';
 import '../../domian/repos/user_profile_repo.dart';
@@ -16,15 +18,29 @@ class UserProfileView extends StatelessWidget {
       body: MultiBlocProvider(
         providers: [
           BlocProvider(
-            create: (context) => UploadUserImageCubit(
-              getIt.get<UserProfileRepo>(),
-            )
-          ),
+              create: (context) => UploadUserImageCubit(
+                    getIt.get<UserProfileRepo>(),
+                  )),
           BlocProvider(
-            create: (context) =>getIt<ImagePickerCubit>(),
+            create: (context) => getIt<ImagePickerCubit>(),
           ),
         ],
-        child: UserProfileViewBody(),
+        child: BlocConsumer<UploadUserImageCubit, UploadUserImageState>(
+          listener: (context, state) {
+            if (state is UploadUserImageSuccess) {
+              buildMessageBar(context, "تم تحديث الصورة بنجاح");
+              Navigator.pop(context);
+            }
+            if (state is UploadUserImageFailure) {
+              buildMessageBar(context, state.errorMessage);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+                inAsyncCall: state is UploadUserImageLoading ? true : false,
+                child: const UserProfileViewBody());
+          },
+        ),
       ),
     );
   }
