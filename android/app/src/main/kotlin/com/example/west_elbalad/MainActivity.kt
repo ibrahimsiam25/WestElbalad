592c5ff93c360c4304a621a2eb94157d6707aa0b
ibrahimsiam25 <ebrahimfathy2822@gmail.com>
2026-02-19 05:31:27 +0200

    Add MainActivity class for Flutter integration

--- android/app/src/main/kotlin/com/example/west_elbalad/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.west_elbalad
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ android/app/src/main/kotlin/com/siam/west_elbalad/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.siam.west_elbalad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
